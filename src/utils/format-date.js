dac260fb8ff06f8bfe89871d1303a410d6515ab5
DanishDude <christian.hinge@gmail.com>
2021-02-04 12:55:22 +0100

    calculate local date time

--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -18,3 +18,28 @@ export function formatDateTime(timestamp) {
     timeZoneName: "short",
   }).format(new Date(timestamp));
 }
+
+export function formatDateTimeLocal(timestamp) {
+  const date = new Date(timestamp);
+  const tl = timestamp.length;
+  const utcOffset = date.getTimezoneOffset();
+  const localOffsetHours = timestamp.slice(tl - 6, tl - 3) * 60;
+  const localOffsetMinutes =
+    timestamp.slice(tl - 6, tl - 5) + timestamp.slice(tl - 2);
+  const localOffset = localOffsetHours + localOffsetMinutes * 1;
+  const sign = localOffset >= 0 ? "+" : "-";
+
+  date.setMinutes(date.getMinutes() + utcOffset + localOffset);
+  const gmtOffsetText = ` GMT${sign}${Math.abs(localOffset / 60)}`;
+
+  return (
+    new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    }).format(new Date(date)) + gmtOffsetText
+  );
+}
