0efd5984b281b69d4296e25bc6a4950c314b960f
DanishDude <christian.hinge@gmail.com>
2021-02-10 20:02:09 +0100

    launch date + site

--- src/components/launches.js
+++ src/components/launches.js
@@ -1,5 +1,22 @@
 import React from "react";
-import { Badge, Box, Image, SimpleGrid, Text, Flex } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Image,
+  Input,
+  Select,
+  SimpleGrid,
+  Stack,
+  Text,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
 import { format as timeAgo } from "timeago.js";
 import { Link } from "react-router-dom";
 
@@ -14,19 +31,23 @@ import FavoritesContext from "../context/favorites-context";
 const PAGE_SIZE = 12;
 
 export default function Launches() {
+  const [options, setOptions] = React.useState({});
   const { data, error, isValidating, setSize, size } = useSpaceXPaginated(
     "/launches/past",
     {
       limit: PAGE_SIZE,
       order: "desc",
       sort: "launch_date_utc",
+      ...options,
     }
   );
+
   return (
     <div>
       <Breadcrumbs
         items={[{ label: "Home", to: "/" }, { label: "Launches" }]}
       />
+      <LaunchSearch setOptions={(opts) => setOptions(opts)} />
       <SimpleGrid m={[2, null, 6]} minChildWidth="350px" spacing="4">
         {error && <Error />}
         {data &&
@@ -43,6 +64,86 @@ export default function Launches() {
         isLoadingMore={isValidating}
       />
     </div>
+  );
+}
+
+function LaunchSearch({ setOptions }) {
+  const [query, setQuery] = React.useState({});
+  const minDate = "2006-01-01";
+  const maxDate = () => {
+    let d = new Date();
+    return d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+  };
+
+  return (
+    <Wrap marginX={6} justify="center">
+      <WrapItem>
+        <FormControl id="start">
+          <FormLabel>From</FormLabel>
+          <Input
+            w={180}
+            type="date"
+            min={minDate}
+            max={maxDate()}
+            onChange={(e) =>
+              setQuery({ end: maxDate(), ...query, start: e.target.value })
+            }
+          />
+        </FormControl>
+      </WrapItem>
+      <WrapItem>
+        <FormControl id="end">
+          <FormLabel>To</FormLabel>
+          <Input
+            w={180}
+            type="date"
+            min={minDate}
+            max={maxDate()}
+            onChange={(e) =>
+              setQuery({ start: minDate, ...query, end: e.target.value })
+            }
+          />
+        </FormControl>
+      </WrapItem>
+      <WrapItem>
+        <FormControl>
+          <FormLabel>Launch Site</FormLabel>
+          <Select
+            w={350}
+            placeholder=" "
+            onChange={(e) =>
+              e.target.value
+                ? setQuery({
+                    start: minDate,
+                    ...query,
+                    site_name: e.target.value,
+                  })
+                : ""
+            }
+          >
+            <option value="KSC LC 39A">
+              Kennedy Space Center (KSC LC 39A)
+            </option>
+            <option value="Kwajalein Atoll">
+              Kwajalein Atoll Omelek Island
+            </option>
+            <option value="CCAFS SLC 40">Cape Canaveral (CCAFS SLC 40)</option>
+            <option value="VAFB SLC 4E">
+              Vandenberg Complex 4E (VAFB SLC 4E)
+            </option>
+            <option value="STLS">SpaceX South Texas (STLS)</option>
+            <option value="VAFB SLC 3W">
+              Vandenberg Complex 3W (VAFB SLC 3W)
+            </option>
+          </Select>
+        </FormControl>
+      </WrapItem>
+      <Box p={4} w="100%" d="flex" justifyContent="center">
+        <Button w={150} onClick={() => setOptions(query)}>
+          Search
+        </Button>
+      </Box>
+    </Wrap>
   );
 }
 
